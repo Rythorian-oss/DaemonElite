d3c05a5ea3daaefea219de49926807299ad22f7c
Justin Linwood Ross <heshlok@proton.me>
2026-08-15 09:34:37 -0400

    Update VoicePreset.cs

--- Models/VoicePreset.cs
+++ Models/VoicePreset.cs
@@ -4,6 +4,26 @@
 // Copyright: (C) 2026 Justin Linwood Ross
 
 // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+#region SYSTEM INITIALIZATION : BLACK STAR PROJECT
+/// <summary>
+/// Core application node for the Black Star Research Facility.
+/// </summary>
+/// <remarks>
+/// <code>
+/// ========================================================================
+///   ____  _        _    ____ _  __  ____ _____  _    ____  
+///  | __ )| |      / \  / ___| |/ / / ___|_   _|/ \  |  _ \ 
+///  |  _ \| |     / _ \| |   | ' /  \___ \ | | / _ \ | |_) |
+///  | |_) | |___ / ___ \ |___| . \   ___) || |/ ___ \|  _ < 
+///  |____/|_____/_/   \_\____|_|\_\ |____/ |_/_/   \_\_| \_\
+///                                                          
+///              R E S E A R C H   F A C I L I T Y           
+///                                                          
+///             [ LOCATION: ICELAND ]            
+/// ========================================================================
+/// </code>
+/// </remarks>
+#endregion
 namespace DaemonElite.Models
 {
     public sealed record VoicePreset
